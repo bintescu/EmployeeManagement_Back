91372bd6e877930c8003fa03600f720bd43bd245
bintescu <41958601+bintescu@users.noreply.github.com>
2020-08-28 11:08:43 +0300

    StackTrace only in DebugMode

--- src/main/java/com/ausy_technologies/demospring/DemospringApplication.java
+++ src/main/java/com/ausy_technologies/demospring/DemospringApplication.java
@@ -1,16 +1,19 @@
 package com.ausy_technologies.demospring;
 
+import com.ausy_technologies.demospring.Exceptions.ErrorResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class DemospringApplication {
-
+    public static final Logger lgr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static void main(String[] args) {
-
+        ErrorResponse.setupLogger();
         SpringApplication.run(DemospringApplication.class, args);
 
-
     }
+
 
 }
